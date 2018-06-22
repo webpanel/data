51230725b40f0d1684505eeb2669b67108cce06c
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2018-06-22 03:03:23 +0200

    ResourceBase cleanup

--- src/ResourceBase.ts
+++ src/ResourceBase.ts
@@ -19,67 +19,7 @@ export class ResourceBase<T> {
 
   constructor(config: ResourceBaseConfig) {
     this.dataSource = config.dataSource;
-    // this.dataSource = DataSourceProvider.shared().getDataSource(
-    //   config.dataSource
-    // );
     this.name = config.name;
     this.fields = config.fields;
   }
-
-  // get = async () => {
-  //   this.loading = true;
-  //   let res = await this.dataSource.read(
-  //     this.name,
-  //     this.id,
-  //     this.fields,
-  //     this.args || {}
-  //   );
-  //   this.data = res;
-  //   this.loading = false;
-  // };
-  // create = async (values: { [key: string]: any }) => {
-  //   this.loading = true;
-  //   let res = await this.dataSource.create(this.name, values, [
-  //     'id',
-  //     ...this.fields
-  //   ]);
-  //   // console.log('created???', res);
-  //   this.data = res;
-  //   this.id = res.id;
-  //   this.loading = false;
-  // };
-  // update = async (values: { [key: string]: any }) => {
-  //   if (!this.id) {
-  //     throw new Error('resource id is missing');
-  //   }
-  //   this.loading = true;
-  //   let res = await this.dataSource.update(
-  //     this.name,
-  //     this.id,
-  //     values,
-  //     this.fields
-  //   );
-  //   this.data = res;
-  //   this.loading = false;
-  // };
-  // delete = async () => {
-  //   if (!this.id) {
-  //     throw new Error('resource id is missing');
-  //   }
-  //   this.loading = true;
-  //   let res = await this.dataSource.delete(this.name, this.id, this.fields);
-  //   this.data = res;
-  //   this.loading = false;
-  // };
-
-  // isPersisted = (): boolean => {
-  //   return !!this.id;
-  // };
-
-  // save = async (values: { [key: string]: any }) => {
-  //   if (!this.id) {
-  //     return this.create(values);
-  //   }
-  //   return this.update(values);
-  // };
 }
