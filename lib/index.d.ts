2699edfc1e829b09738cc0a5a3c84c4014cc8120
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2018-07-14 11:48:28 +0200

    Update exports

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,11 @@
 export { DataSource } from './DataSource';
+export { DataSourceRequest } from './DataSourceRequest';
 export { ResourceLayer } from './components/ResourceLayer';
 export { ResourceCollectionLayer } from './components/ResourceCollectionLayer';
 export { Resource, ResourceConfig } from './Resource';
 export { ResourceCollection } from './ResourceCollection';
+export { HTTPRequest } from './utils/HTTPRequest';
+export { HTTPResponse } from './utils/HTTPResponse';
 export { ResponseDataTransformer, ResourceResponse, ResourceCollectionResponse } from './connectors/ResponseDataTransformer';
 export { HTTPConnector, HTTPConnectorConfiguration } from './connectors/HTTPConnector';
 export { GraphQLConnector } from './connectors/graphql/GraphQLConnector';
