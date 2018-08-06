b979fd1667de12f3c3e7765b0fa59ec9a0b51504
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2018-08-06 09:36:53 +0200

    Fix graphql mutations

--- lib/connectors/graphql/GraphQLConnector.d.ts
+++ lib/connectors/graphql/GraphQLConnector.d.ts
@@ -6,6 +6,7 @@ export declare type GraphQLFieldSource = {
 export declare type GraphQLFieldSourceMap = GraphQLFieldSource | GraphQLFieldSource[];
 export declare class GraphQLConnector extends HTTPConnector {
     transformResponse(response: HTTPResponse, request: DataSourceRequest): Promise<ResourceResponse | ResourceCollectionResponse | null>;
+    fetchFieldNameForRequest(request: DataSourceRequest): string;
     transformRequest(request: DataSourceRequest): HTTPRequest;
     generateQueryParams(name: string, args: object): string;
     transformData(res: HTTPResponse, request: DataSourceRequest): any;
