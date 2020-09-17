12bb0aacd4e8ed57dab157499f61380f7e564798
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2020-09-17 20:56:39 +0200

    Add resource polling version to enforce refresh

--- src/hooks/use-resource-collection.tsx
+++ src/hooks/use-resource-collection.tsx
@@ -12,20 +12,26 @@ export function useResourceCollection<T extends { id: ResourceID } = any>(
   const [resourceCollection, setCollection] = useState<ResourceCollection<T>>(
     new ResourceCollection(config)
   );
+  const [version, setVersion] = useState(0);
   const [conf, setConf] = useState("");
 
   const stringConf = JSON.stringify(config);
   resourceCollection.onPollHandler = () => {
+    console.log("poll1", version);
     setCollection(resourceCollection);
+    setVersion(new Date().getTime());
   };
   useEffect(() => {
     const load = async () => {
       const newCollection = new ResourceCollection<T>(config);
       newCollection.onPollHandler = () => {
+        console.log("poll2", version);
         setCollection(newCollection);
+        setVersion(new Date().getTime());
       };
       await newCollection.get();
       setCollection(newCollection as ResourceCollection<T>);
+      setVersion(new Date().getTime());
     };
     if (conf !== stringConf) {
       load();
